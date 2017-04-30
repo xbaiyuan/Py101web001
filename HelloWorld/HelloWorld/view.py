0f613b03862e746510b1c46cb722982c8aa1b9dc
Ollie <lixinisme1115@126.com>
2017-04-30 10:25:47 +0800

    move hello world to branch

--- HelloWorld/HelloWorld/view.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.shortcuts import render
-
-def hello(request):
-    context = {}
-    context['hello'] = 'Hello World!'
-    return render(request, 'hello.html', context)
