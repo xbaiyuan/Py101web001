77a5ced67a4cd9b1d8d56965aacc27be033f4af1
Ollie <lixinisme1115@126.com>
2017-05-01 19:53:42 +0800

    replace code and note of Templates(uncompleted)

--- TemplatesSample/TemplatesSample/view.py
+++ TemplatesSample/TemplatesSample/view.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def hello(request):
     context = {}
     context['hello'] = 'Hello World!'
-    return render(request, 'hello.html', context)
+    return render(request, 'index.html', context)
